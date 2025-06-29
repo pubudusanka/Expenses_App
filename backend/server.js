f919d46d81752f4ac8c2b364724dbeffdfa740a2
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-29 14:59:27 +0530

    create post req & test with postman

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+// Middleware
+app.use(express.json()); // To parse JSON bodies
+
 async function initDB(){
     try{
         await sql`CREATE TABLE IF NOT EXISTS transactions (
@@ -26,9 +29,26 @@ async function initDB(){
     }
 }
 
-app.get('/', (req,res) => {
-    console.log('Backend server is running');
-});
+app.post('/api/transactions', async (req,res) => {
+    // title, amount, category, user_id
+    try{
+        const {title, amount, category, user_id} = req.body;
+
+        if(!title || amount === undefined || !category || !user_id){
+            return res.status(400).json({message: 'All fields are required'});
+        }
+
+        const transaction = await sql `
+            INSERT INTO transactions (user_id, title, amount, category)
+            VALUES (${user_id}, ${title}, ${amount}, ${category})
+            RETURNING *`
+        console.log(transaction);    
+        return res.status(201).json(transaction[0]);
+    }catch(err){
+        console.error('Error creating transaction:', err);
+        return res.status(500).json({message: 'Internal server error'});
+    }
+})
 
 initDB().then(() => {
     app.listen(PORT, () => {
