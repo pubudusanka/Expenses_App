693511b96b5a7cf9b2fae86494d6bdfe674230c4
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-29 14:24:21 +0530

    connect db and create table

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,37 @@
 //const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import { sql } from './config/db.js'; // Adjust the import path as necessary
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Hello from the backend!");
+const PORT = process.env.PORT || 5001;
+
+async function initDB(){
+    try{
+        await sql`CREATE TABLE IF NOT EXISTS transactions (
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+        console.log('Database connected and table created successfully');
+    }catch(err){
+        console.error('Error initializing the database:', err);
+        process.exit(1); // Exit the process if there is an error : 1 -> error , 0 -> success
+    }
+}
+
+app.get('/', (req,res) => {
+    console.log('Backend server is running');
 });
 
-console.log(process.env.PORT);
-
-// use port in .env file or default to 5001
-app.listen(process.env.PORT || 5001, () => {
-    console.log(`Server is running on port ${process.env.PORT || 5001}`);
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port', PORT);
+    });
 });
