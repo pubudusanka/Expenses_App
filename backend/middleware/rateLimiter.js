4f882e9a0dbf1ef5f6b79923b473329609dcfd9d
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-29 17:51:24 +0530

    Update rateLimiter.js

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,8 +2,9 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async(req,res, next) => {
     try {
-        // userId, Ip address
-        const {success} = await ratelimit.limit("my-rate-limit")
+        // here we just keep simple
+        // in a real-world-app put user_id or IP address as the key
+        const {success} = await ratelimit.limit("vaulet-rate-limit")
 
         if (!success){
             return res.status(429).json({
