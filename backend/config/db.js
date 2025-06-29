ab605b73dbad6256124e4c76cd2d860fc196600c
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-29 18:39:10 +0530

    db move to config

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,3 +3,20 @@ import "dotenv/config";
 
 // Create a SQL connection using DB URL
 export const sql = neon (process.env.DATABASE_URL)
+
+export async function initDB(){
+    try{
+        await sql`CREATE TABLE IF NOT EXISTS transactions (
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+        console.log('Database connected and table created successfully');
+    }catch(err){
+        console.error('Error initializing the database:', err);
+        process.exit(1); // Exit the process if there is an error : 1 -> error , 0 -> success
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 //const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
-import { sql } from './config/db.js'; // Adjust the import path as necessary
+import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 
 import transactionRoute from "./routes/transactionsRoute.js"
@@ -15,23 +15,6 @@ const PORT = process.env.PORT || 5001;
 // Middleware
 app.use(rateLimiter);
 app.use(express.json()); // To parse JSON bodies
-
-async function initDB(){
-    try{
-        await sql`CREATE TABLE IF NOT EXISTS transactions (
-            id SERIAL PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount DECIMAL(10,2) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`
-        console.log('Database connected and table created successfully');
-    }catch(err){
-        console.error('Error initializing the database:', err);
-        process.exit(1); // Exit the process if there is an error : 1 -> error , 0 -> success
-    }
-}
 
 app.use("/api/transactions",transactionRoute);
 
