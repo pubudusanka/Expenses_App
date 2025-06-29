e9c609729c8e3c80f930d94051a38d7945d356f3
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-29 17:48:42 +0530

    fixing module import error
    using module package extension is required when import local

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -1,11 +1,11 @@
 import { Redis } from '@upstash/redis'
-import { Ratelimit} from '@upstash/ratelimit'
+import { Ratelimit } from '@upstash/ratelimit'
 import "dotenv/config";
 
 const ratelimit = new Ratelimit({
 
-    redis:Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(100,"60 s"), //limit the requests for 60s
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(100, '60 s'), //limit the requests for 60s
 });
 
 export default ratelimit;
